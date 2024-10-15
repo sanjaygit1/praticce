4c212c3a332bd5937906539b63c6bcde03f9875e
sanjaygit1 <rathinreddy14@gmail.com>
2024-10-15 12:59:55 +0530

    one

---
+++ GitBashPtc/src/test/java/demo/Test1.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		
+		//Test case complted
+		//pull git bash
+		//hi
+	}
+
+}
